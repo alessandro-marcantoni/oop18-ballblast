1284c98442bf5767e46821a045bf4649afd07754
Alberto Marfoglia <alberto.marfoglia@studio.unibo.it>
2019-08-04 16:39:06 +0200

    add ShooterComponent

---
+++ src/ballblast/model/components/ShooterComponent.java
@@ -0,0 +1,64 @@
+package ballblast.model.components;
+
+import com.google.common.collect.ImmutableList;
+
+import ballblast.model.gameobjects.Bullet;
+import ballblast.model.gameobjects.GameObjectManager;
+import ballblast.model.physics.CollisionManager;
+import ballblast.model.physics.CollisionTag;
+
+/**
+ * Represents the {@link Component} which allows a {@link GameObject} to shot.
+ *
+ */
+public class ShooterComponent extends AbstractComponent {
+    private final GameObjectManager gameObjectManager;
+    private final CollisionManager collisionManager;
+    private boolean shootingState;
+    /**
+     * Creates a {@link ShooterComponent} instance.
+     * @param gameObjectManager
+     *     the {@link GameObjectManager} used to add {@link Bullet}s.
+     * @param collisionManager
+     *     the {@link CollisionManager} used to create the {@link CollisionComponent}.
+     */
+    public ShooterComponent(final GameObjectManager gameObjectManager, final CollisionManager collisionManager) {
+        super(ComponentTypes.SHOOTER);
+        this.gameObjectManager = gameObjectManager;
+        this.collisionManager = collisionManager;
+    }
+
+    @Override
+    public final void update(final double elapsed) {
+        if (this.shootingState) {
+            this.shoot();
+            this.shootingState = false;
+        }
+    }
+    /**
+     * Sets the {@link ShooterComponent}'s state.
+     * @param shootingState
+     *     the boolean indicates if the {@link Shooter} has to shot.
+     */
+    public void setShootingState(final boolean shootingState) {
+        this.shootingState = shootingState;
+    }
+    /**
+     * Gets the {@link ShooterComponent}'s state.
+     * @return
+     *     the true if the {@link Shooter} has to shot, false otherwise.
+     */
+    public boolean getShootingState() {
+        return this.shootingState;
+    }
+    /**
+     * Creates and adds a {@link Bullet} to the {@link GameObjectManager}. 
+     */
+    private void shoot() {
+        final Bullet bullet = new Bullet.Builder()
+                .setPosition(this.getParent().getPosition())
+                .addComponent(new CollisionComponent(collisionManager, CollisionTag.BULLET))
+                .build();
+        this.gameObjectManager.addGameObjects(ImmutableList.of(bullet));
+    }
+}

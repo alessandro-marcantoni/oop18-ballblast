79db2a808e3b88a9486122e21ff4d049fda44cf2
Elia Marcantognini <elia.marcantognini@studio.unibo.it>
2019-08-11 18:21:56 +0200

    Add methods on view interface

--- src/ballblast/view/View.java
+++ src/ballblast/view/View.java
@@ -1,5 +1,58 @@
 package ballblast.view;
 
+import java.util.List;
+import java.util.Optional;
+
+import ballblast.view.entities.ViewEntity;
+import ballblast.view.scenecontroller.GameSceneController;
+import ballblast.view.scenefactory.SceneFactory;
+
+/**
+ * 
+ * Interface that represents the view of the game.
+ * 
+ */
+
 public interface View {
 
+    /**
+     * 
+     * @param controller the controller
+     */
+    void viewLauncher(Controller controller);
+    
+    /**
+     * 
+     * @return the scene factory.
+     */
+    SceneFactory getSceneFactory();
+    
+    /**
+     * Start a new match.
+     * 
+     * @param gameSceneController the game scene controller
+     */
+    void startGame(GameSceneController gameSceneController);
+    
+    /**
+     * 
+     * @param viewEntities entities in the view
+     */
+    void render(List<Optional<ViewEntity>> viewEntities);
+    
+    /**
+     * Start the view thread.
+     */
+    void startRender();
+    
+    /**
+     * Stop the view thread.
+     */
+    void stopRender();
+   
+    /**
+     * 
+     * @return the controller.
+     */
+    Controller getController();
 }

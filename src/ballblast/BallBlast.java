a095e026efdd9c9a4758edd4194192039a09eb06
Elia Marcantognini <elia.marcantognini@studio.unibo.it>
2019-08-11 22:20:05 +0200

    Add viewImpl and code ballblast

--- src/ballblast/BallBlast.java
+++ src/ballblast/BallBlast.java
@@ -1,9 +1,14 @@
 package ballblast;
 
+import ballblast.view.View;
+import ballblast.view.ViewImpl;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * The class containing the main method to start the application.
  */
-public final class BallBlast {
+public final class BallBlast extends Application{
     private BallBlast() { };
     /**
      * The main entry point of the application.
@@ -12,5 +17,12 @@ public final class BallBlast {
      */
     public static void main(final String [] args) {
         System.out.println("Hello World!\n");
+    }
+    @Override
+    public void start(final Stage PrimaryStage) throws Exception {
+        // final View view = new ViewImpl(PrimaryStage);
+        // final Controller controller = new ControllerImpl(() -> new ModelImpl(), view);
+        // view.viewLauncher(controller);
+        
     }
 }
---
+++ src/ballblast/view/ViewImpl.java
@@ -0,0 +1,42 @@
+package ballblast.view;
+
+import java.util.List;
+import java.util.Optional;
+
+import ballblast.view.entities.ViewEntity;
+import ballblast.view.scenecontroller.GameSceneController;
+import ballblast.view.scenefactory.SceneFactory;
+
+public class ViewImpl implements View {
+
+    @Override
+    public SceneFactory getSceneFactory() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void startGame(GameSceneController gameSceneController) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void render(List<Optional<ViewEntity>> viewEntities) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void startRender() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void stopRender() {
+        // TODO Auto-generated method stub
+
+    }
+
+}

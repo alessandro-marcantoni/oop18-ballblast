9b507a4734521d29e8c0df4ce32653f0763e1ac2
Elia Marcantognini <elia.marcantognini@studio.unibo.it>
2019-08-11 23:23:26 +0200

    Add observer interface

---
+++ src/ballblast/settings/observers/Observer.java
@@ -0,0 +1,15 @@
+package ballblast.settings.observers;
+
+/**
+ * 
+ * 
+ * Interface
+ * 
+ */
+public interface Observer {
+
+    /**
+     * A method which notify that something has change.
+     */
+    void update();
+}

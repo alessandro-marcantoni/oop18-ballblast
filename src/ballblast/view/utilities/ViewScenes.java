c7aff683076a7e26f5593d060b56a09f508f02b0
Elia Marcantognini <elia.marcantognini@studio.unibo.it>
2019-08-11 19:21:37 +0200

    Modify ViewScenes

--- src/ballblast/view/utilities/ViewScenes.java
+++ src/ballblast/view/utilities/ViewScenes.java
@@ -5,10 +5,38 @@ package ballblast.view.utilities;
  * Enumeration that contains the game scenes.
  */
 public enum ViewScenes {
-    /*
+    /**
      * The game scene.
      */
-    GAME("Game.fxml");
+    GAME("Game.fxml"),
+    /**
+     * The main menu scene.
+     */
+    MENU("Menu.fxml"),
+    /**
+     * The pause scene.
+     */
+    PAUSE("Pause.fxml"),
+    /**
+     * The settings scene.
+     */
+    SETTINGS("Settings.fxml"),
+    /**
+     * The leaderboard scene.
+     */
+    LEADERBOARD("Leaderboard.fxml"),
+    /**
+     * The manual scene.
+     */
+    MANUAL("Manual.fxml"),
+    /**
+     * The gameover scene.
+     */
+    GAMEOVER("Gameover.fxml");
+    //LOGIN
+    //REGISTER
+    //SELECTMODE
+    
     
     private static final String PATH = "/view/scenes";
     private final String selectedScene;

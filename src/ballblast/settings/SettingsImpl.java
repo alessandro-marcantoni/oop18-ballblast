5d007e201f5572da9d85822dd07c6a3d435e5722
Elia Marcantognini <elia.marcantognini@studio.unibo.it>
2019-08-11 23:20:14 +0200

    Implement settings

--- src/ballblast/settings/SettingsImpl.java
+++ src/ballblast/settings/SettingsImpl.java
@@ -1,5 +1,8 @@
 package ballblast.settings;
 
+import java.util.Objects;
+
+import javafx.stage.Screen;
 import javafx.util.Pair;
 /**
  * 
@@ -8,34 +11,57 @@ import javafx.util.Pair;
  */
 public class SettingsImpl implements Settings {
 
+    private static final int PREF_X_RES = 1920;
+    private static final int PREF_Y_RES = 1080;
+    private static final int DEFAULT_X_RES = (int) Screen.getPrimary().getBounds().getWidth();
+    private static final int DEFAULT_Y_RES = (int) Screen.getPrimary().getBounds().getHeight();
+    private static final Pair<Integer, Integer> DEFAULT_RES = new Pair<>(DEFAULT_X_RES, DEFAULT_Y_RES);
+    
+    private static final Pair<Integer, Integer> SCREEN_RES = DEFAULT_RES;
+    
+    private final Pair<Integer, Integer> selectedResolution = new Pair<>(SettingsImpl.SCREEN_RES.getKey(), 
+                                                                         SettingsImpl.SCREEN_RES.getValue());
+    
+    private boolean fullScreen = true;
+    
+    private static SettingsImpl singleton;
+          
+    /**
+     * 
+     * @return
+     *          Instance of this object, only once.
+     */
+    public static SettingsImpl getSettings() {
+        if (Objects.isNull(singleton)) {
+            singleton = new SettingsImpl();
+        }
+        return singleton;
+    }
+    
     @Override
     public Pair<Integer, Integer> getDefaultResolution() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.DEFAULT_RES;
     }
 
     @Override
     public Pair<Integer, Integer> getSelectedResolution() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.selectedResolution;
     }
 
     @Override
     public double getScaleFactor() {
-        // TODO Auto-generated method stub
-        return 0;
+        return Math.min((double) this.selectedResolution.getValue()/SettingsImpl.PREF_Y_RES,
+                        (double) this.selectedResolution.getKey()/SettingsImpl.PREF_X_RES);
     }
 
     @Override
     public boolean isFullScreen() {
-        // TODO Auto-generated method stub
-        return false;
+        return this.fullScreen;
     }
 
     @Override
     public void setFullScreen(boolean fullScreen) {
-        // TODO Auto-generated method stub
-
+        this.fullScreen = fullScreen;
     }
 
 }

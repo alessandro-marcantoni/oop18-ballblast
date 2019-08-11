b07a31b1af7a6c23cb4d10b8cd0d241021e94e78
Elia Marcantognini <elia.marcantognini@studio.unibo.it>
2019-08-11 18:34:52 +0200

    Add methods on interfaces ViewEntitity and GameSceneController

--- src/ballblast/view/entities/ViewEntity.java
+++ src/ballblast/view/entities/ViewEntity.java
@@ -1,5 +1,32 @@
 package ballblast.view.entities;
 
-public interface ViewEntity {
+import org.locationtech.jts.geom.Coordinate;
 
+import javafx.scene.image.Image;
+import javafx.scene.shape.Shape;
+
+/**
+ * 
+ * Represent the entity's information to draw in the view.
+ * 
+ */
+public interface ViewEntity {
+    
+    /**
+     * 
+     * @return the entity position.
+     */
+    Coordinate getPosition();
+    
+    /**
+     * 
+     * @return the entity shape.
+     */
+    Shape getShape();
+    
+    /**
+     * 
+     * @return the entity image.
+     */
+    Image getPicture();
 }
--- src/ballblast/view/scenecontroller/GameSceneController.java
+++ src/ballblast/view/scenecontroller/GameSceneController.java
@@ -1,5 +1,18 @@
 package ballblast.view.scenecontroller;
 
+import javafx.scene.canvas.Canvas;
+
+/**
+ * 
+ * Interface that represent the game scene controller.
+ * 
+ */
 public interface GameSceneController {
 
+    /**
+     * 
+     * @return the game page canvas.
+     */
+    Canvas getCanvas();
+    
 }

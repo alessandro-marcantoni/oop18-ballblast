af589327a943425a18d1b34f8763b4784d46df1d
Elia Marcantognini <elia.marcantognini@studio.unibo.it>
2019-08-12 23:08:05 +0200

    Modified manualcontroller

--- src/ballblast/view/scenecontroller/ManualSceneControllerImpl.java
+++ src/ballblast/view/scenecontroller/ManualSceneControllerImpl.java
@@ -5,6 +5,7 @@ package ballblast.view.scenecontroller;
  * Controller implementation for the manual scene.
  * 
  */
+
 public class ManualSceneControllerImpl extends AbstractSubSceneController {
     /**
      * No other methods needed.

192ec1fa10f701aff4b3a6e89a1402350c1edb9f
Elia Marcantognini <elia.marcantognini@studio.unibo.it>
2019-08-12 17:03:25 +0200

    Create abstract class for secondary menus.

---
+++ src/ballblast/view/scenecontroller/AbstractSubSceneController.java
@@ -0,0 +1,18 @@
+package ballblast.view.scenecontroller;
+
+import javafx.fxml.FXML;
+
+/**
+ * 
+ * Abstract class that add method for secondary menus.
+ */
+public abstract class AbstractSubSceneController extends AbstractSceneController {
+
+    /**
+     * Return to main menu scene.
+     */
+    @FXML
+    protected void backToMenu() {
+        super.getSceneFactory().openMenuScene();
+    }
+}

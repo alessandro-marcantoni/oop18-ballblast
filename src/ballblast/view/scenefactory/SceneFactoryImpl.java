486ade5df61ca5c718da07cc7c0ac8fc7d9f9462
Elia Marcantognini <elia.marcantognini@studio.unibo.it>
2019-08-12 17:16:52 +0200

    Modify some code on scenefactoryimpl.

--- src/ballblast/view/scenefactory/SceneFactoryImpl.java
+++ src/ballblast/view/scenefactory/SceneFactoryImpl.java
@@ -1,7 +1,10 @@
 package ballblast.view.scenefactory;
 
+import ballblast.settings.SettingsImpl;
 import ballblast.view.View;
 import ballblast.view.sceneloader.SceneLoader;
+import ballblast.view.sceneloader.SceneLoaderImpl;
+import ballblast.view.utilities.ViewScenes;
 import javafx.stage.Stage;
 
 /**
@@ -15,23 +18,32 @@ public class SceneFactoryImpl implements SceneFactory {
     private final View view;
     private final SceneLoader sceneLoader;
     
+    /**
+     * Public constructor for SceneFactory.
+     * 
+     * @param view
+     *          the view of the game.
+     */
+    public SceneFactoryImpl(View view) {
+        this.view = view;
+        this.sceneLoader = new SceneLoaderImpl(this.view);
+    }
+
     @Override
     public void setStage(Stage stage) {
-        // TODO Auto-generated method stub
-
+        this.stage = stage;
     }
 
     @Override
     public Stage getStage() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.stage;
     }
 
     @Override
-    public void openMenuScene() {
-        // TODO Auto-generated method stub
-
+    public final void openMenuScene() {
+        this.openNewScene(ViewScenes.MENU);
     }
+
 
     @Override
     public void openSettingsScene() {
@@ -61,6 +73,25 @@ public class SceneFactoryImpl implements SceneFactory {
     public void openPauseScene() {
         // TODO Auto-generated method stub
 
+    }
+
+    @Override
+    public void openGameSelection() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    private void openNewScene(ViewScenes scene) {
+        this.checkFullScreen();
+        this.sceneLoader.loadScene(this.stage, scene);
+    }
+
+    private void checkFullScreen() {
+        // DA IMPLEMENTARE
+    }
+    
+    private void createNewStage() {
+        // DA IMPLEMENTARE
     }
 
 }

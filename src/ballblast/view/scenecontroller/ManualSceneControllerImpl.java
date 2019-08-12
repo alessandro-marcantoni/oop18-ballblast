1f63044ddbc7ed95724adf2804913f569192d0db
Elia Marcantognini <elia.marcantognini@studio.unibo.it>
2019-08-12 22:04:44 +0200

    Written Manual scene controller, but no methods are needed yet

--- src/ballblast/view/scenecontroller/ManualSceneControllerImpl.java
+++ src/ballblast/view/scenecontroller/ManualSceneControllerImpl.java
@@ -2,8 +2,11 @@ package ballblast.view.scenecontroller;
 
 /**
  * 
+ * Controller implementation for the manual scene.
  * 
  */
 public class ManualSceneControllerImpl extends AbstractSubSceneController {
-    
+    /**
+     * No other methods needed.
+     */
 }

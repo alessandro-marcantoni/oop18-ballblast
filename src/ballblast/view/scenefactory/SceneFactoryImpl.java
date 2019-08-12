01dceb0c36e2ca1addc77f6e5f1d79c155b96834
Elia Marcantognini <elia.marcantognini@studio.unibo.it>
2019-08-12 14:29:57 +0200

    Create SceneFactoryImpl

---
+++ src/ballblast/view/scenefactory/SceneFactoryImpl.java
@@ -0,0 +1,55 @@
+package ballblast.view.scenefactory;
+
+import javafx.stage.Stage;
+
+public class SceneFactoryImpl implements SceneFactory {
+
+    @Override
+    public void setStage(Stage stage) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Stage getStage() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void openMenuScene() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void openSettingsScene() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void openLeaderboardScene() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void openManualScene() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void openGameOverScene() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void openPauseScene() {
+        // TODO Auto-generated method stub
+
+    }
+
+}

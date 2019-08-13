c8bef886f070ba4fe1075cff7ea3ddc85acb755f
Elia Marcantognini <elia.marcantognini@studio.unibo.it>
2019-08-13 20:17:25 +0200

    Change name to viewentityball.

--- src/ballblast/view/entities/ViewEntityBall.java
+++ src/ballblast/view/entities/ViewEntityBall.java
@@ -4,12 +4,12 @@ import ballblast.model.gameobjects.BallTypes;
 import javafx.scene.image.Image;
 import javafx.scene.shape.Shape;
 
-public class Ball extends ViewEntityImpl {
+public class ViewEntityBall extends ViewEntityImpl {
 
     private BallTypes dimension;
     private BallColors color;
     
-    public Ball(final BallTypes dimension, final BallColors color, Shape shape, Image image) {
+    public ViewEntityBall(final BallTypes dimension, final BallColors color, Shape shape, Image image) {
         super(shape, image);
         this.dimension = dimension;
         this.color = BallColors.randomColor();

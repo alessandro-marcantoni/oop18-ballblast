3c815cfc69a41c6d9ff3f7442e184960c8596dc1
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2019-08-13 11:22:04 +0200

    Implement component Gravity

--- src/ballblast/model/components/GravityComponent.java
+++ src/ballblast/model/components/GravityComponent.java
@@ -9,16 +9,48 @@ public class GravityComponent extends AbstractComponent {
 
     private static final Vector2D STANDARD_GRAVITY = Vector2D.create(0, -100);
 
+    private Vector2D gravity;
+
     /**
      * Creates a new GravityComponent instance.
      */
     public GravityComponent() {
         super(ComponentTypes.GRAVITY);
+        this.gravity = STANDARD_GRAVITY;
+    }
+
+    /**
+     * Creates a new GravityComponent instance with specified gravity acceleration.
+     * @param gravity
+     *          the gravity acceleration.
+     */
+    public GravityComponent(final Vector2D gravity) {
+            super(ComponentTypes.GRAVITY);
+            this.gravity = gravity;
     }
 
     @Override
-    public void update(final double elapsed) {
+    public final void update(final double elapsed) {
+        final Vector2D dV = gravity.multiply(elapsed);
+        this.getParent().setVelocity(this.getParent().getVelocity().sumVector(dV));
+    }
 
+    /**
+     * Gets the gravity value.
+     * @return
+     *          the gravity value.
+     */
+    public Vector2D getGravity() {
+            return this.gravity;
+    }
+
+    /**
+     * Sets a new gravity value.
+     * @param gravity
+     *          the gravity vector.
+     */
+    public void setGravity(final Vector2D gravity) {
+            this.gravity = gravity;
     }
 
 }

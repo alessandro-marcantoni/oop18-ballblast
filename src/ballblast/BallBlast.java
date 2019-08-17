f4e23a28ba7e741b28deda06ba2b066086eb24b7
Elia Marcantognini <elia.marcantognini@studio.unibo.it>
2019-08-17 17:56:15 +0200

    written launch() method on BallBlast.

--- src/ballblast/BallBlast.java
+++ src/ballblast/BallBlast.java
@@ -1,5 +1,8 @@
 package ballblast;
 
+import ballblast.controller.Controller;
+import ballblast.view.View;
+import ballblast.view.ViewImpl;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -11,9 +14,9 @@ public final class BallBlast extends Application {
 
     @Override
     public void start(final Stage stage) throws Exception {
-        //final View view = new ViewImpl(stage);
-        //final Controller controller = new ControllerImpl(() -> new ModelImpl(), view);
-        //view.viewLauncher(controller);
+        final View view = new ViewImpl(stage);
+        //final Controller controller = new ControllerImpl(model, view);
+        //view.launch(controller);
     }
     /**
      * The main entry point of the application.

1f01c627a6e7c2e3da241070da60eef57e5487cd
Alberto Marfoglia <alberto.marfoglia@studio.unibo.it>
2019-09-06 12:28:33 +0200

    add the functional interface Command

--- src/ballblast/commons/Command.java
+++ src/ballblast/commons/Command.java
@@ -1,15 +1,17 @@
 package ballblast.commons;
 
 /**
- * 
- * @author albiunix
+ * It is used to wrap a request under an object as command and passed to invoker
+ * object, followion the Command pattern.
  *
- * @param <T>
+ * @param <T> the type of the object used to execute the command.
  */
 @FunctionalInterface
 public interface Command<T> {
     /**
      * Executes the command.
+     * 
+     * @param t the object used to handle this command.
      */
     void execute(T t);
 }
--- src/ballblast/commons/TripleConsumer.java
+++ src/ballblast/commons/TripleConsumer.java
@@ -1,7 +1,7 @@
 package ballblast.commons;
 
 /**
- * This is a functional interface and can therefore be used as the assignment
+ * Is a functional interface and can therefore be used as the assignment
  * target for a lambda expression or method reference.
  * 
  * @param <A> the first type of the input to the operation.
